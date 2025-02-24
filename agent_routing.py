70a3519d463fbb7d2e66e0355b11e1e02a3954c3
ben <Lim6112j@gmail.com>
2025-02-24 11:47:00 +0900

    refactor with closure

--- agent_routing.py
+++ agent_routing.py
@@ -1,15 +1,11 @@
 from typing import Annotated, Union, Dict, List
 from typing_extensions import TypedDict
 from langgraph.graph.message import add_messages
-from langgraph.types import Command
 
-class State(TypedDict):
-    messages: Annotated[list, add_messages]
-    routes: list
 
 from langchain_core.messages import ToolMessage, SystemMessage
 from langchain_core.tools import InjectedToolCallId, tool
-from langgraph.types import Command, interrupt
+from langgraph.types import interrupt
 
 
 from langchain_anthropic import ChatAnthropic
@@ -19,59 +15,14 @@ from langgraph.graph import StateGraph, START, END
 from langgraph.prebuilt import ToolNode, tools_condition
 from dotenv import load_dotenv
 load_dotenv()
-import pandas as pd
-import json
+
 import os
-df = pd.read_csv("./data/locations.csv")
-names = df["name"].tolist()
-lons = df["longitude"].tolist()
-lats = df["latitude"].tolist()
-loc_dict = {names[i]: (lons[i], lats[i]) for i in range(0, len(names))}
+from custom_tool.tools import State, get_routes
+from messages.messages import template
+from helper.funcs import get_messages_info
+
+
 routes = [{"legs": [{"steps": [{"maneuver": {"location": [1, 2]}}] }]}]
-@tool
-def get_routes(state: State, start: str, destination: str,  tool_call_id: Annotated[str, InjectedToolCallId]):
-    """ Get route information from start to destination.
-    after gather start and destination from user,
-    call this function for route data
-    Args:
-       start (str): start location name
-       destination (str): destination location name
-    """
-    start_loc = str(loc_dict[start][0]) + ',' + str(loc_dict[start][1])
-    destination_loc = str(loc_dict[destination][0]) + ',' + str(loc_dict[destination][1])
-    locations = start_loc + ';' + destination_loc
-#    print(locations)
-
-    import http.client
-    OSRM_API = os.getenv("OSRM_API")
-    # http://localhost:5001/route/v1/driving/127.919323,36.809656;128.080629,36.699223?steps=true
-    conn = http.client.HTTPConnection(OSRM_API, 5001)
-    conn.request("GET", "/route/v1/driving/" + locations + "?steps=true")
-    res = conn.getresponse()
-    routes_bytes = res.read()
-    conn.close()
-#    print(f"osrm response routes data {routes}")
-    if not routes_bytes:
-        return "No routes found."
-    # response = "\n".join([f"{route['id']}: {route['title']}" for route in routes])
-
-    json_str = routes_bytes.decode('utf8').replace("'", '"')
-    json_routes = json.loads(json_str)
-    routes = json_routes['routes']
-
-#    print(json_routes['routes'])
-#    print(f"\nlength of routes : {len(json_routes['routes'])}")
-#    fig = draw_route(loc_dict[start], loc_dict[destination])
-#    fig.show()
-#    return f"Available routes:\n{routes}"
-    return Command(
-         update={
-             "routes": routes,
-             "messages": [
-                 ToolMessage(routes_bytes, tool_call_id=tool_call_id)
-             ],
-         }
-    )
 
 tool = TavilySearchResults(max_results=2)
 tools = [get_routes]
@@ -81,32 +32,10 @@ LLM_MODEL = os.getenv("LLM_MODEL")
 llm = ChatOllama(model="llama3.2", temperature=0) if LLM_MODEL == "llama" else ChatAnthropic(model="claude-3-5-sonnet-20240620")
 llm_with_tools = llm.bind_tools(tools)
 
-# template = """Your job is to get information from a user about two location and to explain the route with tool `get_routes`
-
-# You should get the following information from them:
-
-# - What the objective of the prompt is
-# - What variables will be passed into the prompt template
-# - Any constraints for what the output should NOT do
-# - Any requirements that the output MUST adhere to
-
-# If you are not able to discern this info, ask them to clarify! Do not attempt to wildly guess.
-
-# After you are able to discern all the information, call the relevant tool."""
-
-template = """You are a car navigation guide, Your job is to get information from a user about two location and to explain the route with tool `get_routes`
-
-If you are not able to discern two location name, ask them to clarify! Do not attempt to wildly guess.
-
-After you are able to discern all the information, call the relevant tool."""
-
-def get_messages_info(messages):
-    return [SystemMessage(content=template)] + messages
-
 def chatbot(state: State):
     messages = get_messages_info(state["messages"])
 #    print(f"\nstate[messages] => {state['messages']}\n")
-#    print(f"\n[messages] => {messages}\n")
+    print(f"\n[messages] => {messages}\n")
     message = llm_with_tools.invoke(messages)
     assert len(message.tool_calls) <= 1
     return {"messages": [message]}
@@ -126,6 +55,7 @@ graph_builder.add_edge(START, "chatbot")
 
 memory = MemorySaver()
 graph = graph_builder.compile(checkpointer=memory)
+
 def stream_graph_updates(message: str, config: Dict):
     result: str
     for event in graph.stream(
@@ -154,113 +84,12 @@ def run_agent(audio_input, session_id: Union[str, None] = None, thread_id: Union
     return transcription, stream_graph_updates(transcription, config)
 
 import gradio as gr
-
-# airbnb data
-from datasets import load_dataset
+from helper.gradio_func import filter_map
 import plotly.graph_objects as go
-import pandas as pd
-
-df = pd.read_csv("./data/locations.csv")
-def draw_route(start, destination):
-
-    fig = go.Figure().add_trace(go.Scattermapbox(
-        mode='lines',
-        lon = [start[0], destination[0]],
-        lat = [start[1], destination[1]],
-        line_color='green',
-        name='routes calculated'
-    ))
-    fig.update_layout(
-        mapbox_style="open-street-map",
-        hovermode='closest',
-        mapbox=dict(
-            bearing=0,
-            center=go.layout.mapbox.Center(
-                lat=37.497467,
-                lon=127.027458
-            ),
-            pitch=0,
-            zoom=15
-        ),
-    )
-    return fig
-def draw_route_list():
-# loc_list : routes
-    config = {"configurable": {"thread_id": "thread_1"}}
-    state = graph.get_state(config)
-
-
-
-    try:
-        routes = state.values['routes']
-        print(f"\nstate => {routes}\n")
-        loc_list = routes[0]
-        print(f"routes[0] : {loc_list}")
-        lons = []
-        lats = []
-        for leg in loc_list['legs']:
-            for step in leg['steps']:
-                #           print(f"maneuver data : {step[''maneuver']}")
-                location = (step['maneuver'])['location']
-                lons.append(location[0])
-                lats.append(location[1])
-                print(f"\nlats from routes data => {lats}\n")
-                fig = go.Figure().add_trace(go.Scattermapbox(
-                    mode='lines',
-                    lon = lons,
-                    lat = lats,
-                    line_color='green',
-                    name='routes calculated'
-                ))
-                fig.update_layout(
-                    mapbox_style="open-street-map",
-                    hovermode='closest',
-                    mapbox=dict(
-                        bearing=0,
-                        center=go.layout.mapbox.Center(
-                            lat=37.497467,
-                            lon=127.027458
-                        ),
-                        pitch=0,
-                        zoom=13
-                    ),
-                )
-        return fig
-    except:
-        print("routes not ready")
-def filter_map():
-    names = df["name"].tolist()
-    lons = df["longitude"].tolist()
-    lats = df["latitude"].tolist()
-    loc_list = [(names[i], (lons[i], lats[i])) for i in range(0, len(names))]
-    # map figure
-    fig = go.Figure(go.Scattermapbox(
-        customdata=loc_list,
-        lat=lats,
-        lon=lons,
-        mode='markers',
-        marker=go.scattermapbox.Marker(
-            size=6
-        ),
-        hoverinfo="text",
-        hovertemplate='<b>Name</b>: %{customdata[0]}<br><b>Loc</b>: $%{customdata[1]}'
-    ))
-
-    fig.update_layout(
-        mapbox_style="open-street-map",
-        hovermode='closest',
-        mapbox=dict(
-            bearing=0,
-            center=go.layout.mapbox.Center(
-                lat=37.497467,
-                lon=127.027458
-            ),
-            pitch=0,
-            zoom=13
-        ),
-    )
-    return fig
+from helper.gradio_func import draw_route_list_closure
 # ui draw
+def draw_route_list():
+    return draw_route_list_closure(graph)
 with gr.Blocks() as demo:
     gr.Markdown("""# Ciel AI Agent for routing with voice""")
     with gr.Row():

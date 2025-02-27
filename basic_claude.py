01b28e2932ce688aa39360a02c28943ef957f71b
ben <Lim6112j@gmail.com>
2025-02-27 16:45:48 +0900

    human assistance successful

--- basic_claude.py
+++ basic_claude.py
@@ -38,7 +38,7 @@ def chatbot(state: State):
     # we disable parallel tool calling to avoid repeating any
     # tool invocations when we resume.
     message = llm_with_tools.invoke(state["messages"])
-#    assert len(message.tool_calls) <= 1
+    assert len(message.tool_calls) <= 1
     return {"messages": [message]}
 
 
@@ -55,32 +55,41 @@ graph_builder.add_conditional_edges("chatbot", tools_condition)
 graph_builder.add_edge("tools", "chatbot")
 graph_builder.add_edge(START, "chatbot")
 
-graph = graph_builder.compile()
-def stream_graph_updates(user_input: str):
-    for event in graph.stream({"messages": [{"role": "user", "content": user_input}]}):
-        for value in event.values():
-            print("Assistant:", value["messages"][-1].content)
-#def stream_graph_updates(user_input: str):
-#    for event in graph.stream(
-#            {"messages": [{"role": "user", "content": user_input}]},
-#            config,
-#            stream_mode="values"
-#    ):
-#        if "messages" in event:
-#            event["messages"][-1].pretty_print()
-#            result = event["messages"][-1]
-#    return "", [{"role": "user", "content": result.content}]
+memory = MemorySaver()
+graph = graph_builder.compile(checkpointer=memory)
 
+def stream_graph_updates(user_input: str, config: str):
+    for event in graph.stream(
+            {"messages": [{"role": "user", "content": user_input}]},
+            config,
+            stream_mode="values"
+    ):
+        if "messages" in event:
+            event["messages"][-1].pretty_print()
+            result = event["messages"][-1]
+            for item in result.content:
+                if "name" in item and item["name"] == "human_assistance":
+                    print(f"item name => {item['name']}")
+                    human_command = Command(resume={"data": "We, the experts are here to help! We'd recommend you check out LangGraph to build your agent. It's much more reliable and extensible than simple autonomous agents."})
+                    inner_events = graph.stream(human_command, config, stream_mode="values")
+                    for e in inner_events:
+                        if "messages" in e:
+                            e["messages"][-1].pretty_print()
+#            print(f"result => {result}")
+
+    return [{"role": "user", "content": result.content}]
+config = {"configurable": {"thread_id": "1"}}
 while True:
     try:
         user_input = input("User: ")
         if user_input.lower() in ["quit", "exit", "q"]:
             print("Goodbye!")
             break
-        stream_graph_updates(user_input)
+        stream_graph_updates(user_input, config)
     except:
         # fallback if input() is not available
-        user_input = "What do you know about LangGraph?"
+        user_input = "I need some expert guidance for building an AI agent. Could you request assistance for me?"
         print("User: " + user_input)
-        stream_graph_updates(user_input)
+
+        stream_graph_updates(user_input, config)
         break

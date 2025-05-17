2b5df9abef1117f8d92f541cf5b247ba1bd81e06
ben <Lim6112j@gmail.com>
2025-05-17 12:11:21 +0900

    llama3.2

--- agent_dashboard.py
+++ agent_dashboard.py
@@ -61,7 +61,7 @@ LLM_MODEL = os.getenv("LLM_MODEL")
 #    model="claude-3-5-sonnet-20240620")
 
 # llm = ChatOpenRouter(model_name="anthropic/claude-3.7-sonnet:thinking")
-llm = ChatOllama(model="mixtral:latest", temperature=0)
+llm = ChatOllama(model="llama3.2:latest", temperature=0)
 llm_with_tools = llm.bind_tools(tools)
 
 # chatbot node

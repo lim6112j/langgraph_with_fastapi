2bd60a1f33fce3346836f0de52f94878fda9d2b4
ben <Lim6112j@gmail.com>
2025-05-17 11:54:27 +0900

    tool supported model

--- agent_dashboard.py
+++ agent_dashboard.py
@@ -61,7 +61,7 @@ LLM_MODEL = os.getenv("LLM_MODEL")
 #    model="claude-3-5-sonnet-20240620")
 
 # llm = ChatOpenRouter(model_name="anthropic/claude-3.7-sonnet:thinking")
-llm = ChatOllama(model="gemma3:27b", temperature=0)
+llm = ChatOllama(model="qwen3:235b-a22b", temperature=0)
 llm_with_tools = llm.bind_tools(tools)
 
 # chatbot node

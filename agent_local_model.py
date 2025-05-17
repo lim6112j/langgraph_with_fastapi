374197cf07c29c9c09eab1205d62dbbf908cfc18
ben <Lim6112j@gmail.com>
2025-05-17 14:46:08 +0900

    polished

--- agent_local_model.py
+++ agent_local_model.py
@@ -1,17 +1,13 @@
-from typing import Annotated
-from typing_extensions import TypedDict
 from langgraph.graph import StateGraph, START
-from langgraph.graph.message import add_messages
 from langchain_ollama import ChatOllama
-from langchain.schema import AIMessage  # Ensure this import is present
-
-
-class State(TypedDict):
-    messages: Annotated[list, add_messages]
-
+from custom_tool.tools import State, get_data_from_site
+from langgraph.prebuilt import ToolNode, tools_condition
 
 graph_builder = StateGraph(State)
 llm = ChatOllama(model="llama3.2:latest", temperature=0)
+
+tools = [get_data_from_site]
+llm_with_tools = llm.bind_tools(tools)
 
 
 def chatbot(state: State):
@@ -19,6 +15,14 @@ def chatbot(state: State):
 
 
 graph_builder.add_node("chatbot", chatbot)
+tool_node = ToolNode(tools=tools)
+graph_builder.add_node("tools", tool_node)
+
+graph_builder.add_conditional_edges(
+    "chatbot",
+    tools_condition,
+)
+graph_builder.add_edge("tools", "chatbot")
 graph_builder.add_edge(START, "chatbot")
 graph = graph_builder.compile()
 
@@ -26,11 +30,7 @@ graph = graph_builder.compile()
 def stream_graph_updates(user_input: str):
     for event in graph.stream({"messages": [{"role": "user", "content": user_input}]}):
         if "chatbot" in event:
-            # Check if the event contains messages
-
-            # event["chatbot"]["messages"][-1].pretty_print()
             result = event["chatbot"]["messages"]
-            # Check if message is an instance of AIMessage
             print("Assistant:", result.content)
         else:
             print("No messages in event:", event)
@@ -43,9 +43,12 @@ while True:
             print("Goodbye!")
             break
         stream_graph_updates(user_input)
-    except:
+    except Exception as e:
         # fallback if input() is not available
         user_input = "What do you know about LangGraph?"
-        print("User: " + user_input)
+        print("User: " + user_input + "Exception", e)
         stream_graph_updates(user_input)
         break
+    finally:
+        # cleanup if needed
+        pass
